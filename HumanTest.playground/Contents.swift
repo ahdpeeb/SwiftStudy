0f57bbbea4110cc0edca8eb86b8e95bfc1290ee5
Nickola Andriev <ahdpeeb.h@gmail.com>
2016-10-20 18:10:42 +0300

    HumanTest loaded

---
+++ HumanTest.playground/Contents.swift
@@ -0,0 +1,37 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Foundation
+
+class Human {
+    var fullName: String! = nil
+    var age = 0
+    var children: [Human] = [Human]()
+    
+    init() {
+        
+    }
+    
+    init(fullName: String, age: Int) {
+        self.fullName = fullName
+        self.age = age
+    }
+    
+    func sayPhrase(phrase: String) -> (){
+        print("\(self) say \(phrase)")
+    }
+    
+    func createChild(child: Human) -> (Human){
+        let person = Human()
+        self.children += [person]
+        
+        return person
+    }
+}
+
+var bliaMan = Human(fullName: "Pizdec", age: 23)
+bliaMan.sayPhrase(phrase: "Dcem pizdec")
+
+var child = bliaMan.createChild(child: Human(fullName: "Bob", age: 1))
+
+print(bliaMan.children.count)
